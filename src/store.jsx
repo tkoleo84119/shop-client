bb7db6f57810df41033f7aede23613cd67084bb1
Kerwin <tkoleo84119@gmail.com>
2022-04-06 16:31:24 +0800

    feat: add preloadedAuthState from localStorage

--- src/store.jsx
+++ src/store.jsx
@@ -3,6 +3,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import { authReducer as auth } from './reducers/authReducer'
 import { statusReducer as status } from './reducers/statusReducer'
 
+const preloadedAuthState = () => {
+  const auth = localStorage.getItem('auth') ? JSON.parse(localStorage.getItem('auth')) : {}
+  return auth
+}
+
 export default configureStore({
-  reducer: { auth, status }
+  reducer: { auth, status },
+  preloadedState: {
+    auth: preloadedAuthState()
+  }
 })
